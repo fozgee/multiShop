3ec8eb419b74cbae3c425d678fddcd75110c1a7e
fozg <fozg@DESKTOP-EOV61H7>
2016-05-12 22:54:00 +0700

    init files

---
+++ App_Code/Service.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    public List<Work> DoWork()
+    {
+        Work[] w = new Work[2];
+        w[0] = new Work();
+        w[0].name = "aaaa";
+        w[0].age = 123;
+        w[1] = new Work();
+        w[1].name = "bbb";
+        w[1].age = 321;
+        return w.ToList();
+    }
+
+    public static List<string> myValues = new List<string>()
+    {
+        "Apple", "Orange", "Apricot", "Banana", "Grape", "Peach", "Plum"
+    };
+
+
+    public List<Work> GetNav()
+    {
+      
+        using (MShopDataContext context = new MShopDataContext())
+       {
+            
+                var categories = (from row in context.Categories select new Work( row.name , row.id)).ToList();
+
+
+
+                return categories;            
+
+        }
+        //var items = (from v in myValues
+
+        //             select v);
+
+        //return items;
+
+
+    }
+
+ 
+
+}
+
+public class Work
+{
+    public Work() { }
+    public Work(string name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public string name { set; get; }
+    public int age { set; get; }
+}
+
---
+++ multiShopDB.sql
@@ -0,0 +1,36 @@
+﻿CREATE DATABASE multiShop
+--DROP DATABASE multiShop
+go
+USE multiShop
+go
+CREATE TABLE Shop 
+(
+	id int primary key identity,
+	name nvarchar(100),
+	href nvarchar(100)
+)
+CREATE TABLE Category
+(
+	id int primary key identity,
+	shop int references shop(id),
+	name nvarchar(100), 
+	navlink nvarchar(100) 
+)	
+
+CREATE TABLE Product
+(
+	id int primary key identity,
+	name nvarchar(300),
+	descrip nvarchar(max),
+	fulldescrip nvarchar(max),
+	buylink nvarchar(300),
+	category int references category(id)
+)
+
+insert into  Shop values ('Macbook Viet','http://localhost:50295/')
+
+insert into category values (1,N'Sạc','Sac')
+insert into category values (1,N'Phụ kiện','Phukien')
+insert into category values (1,N'Bàn phím','Banphim')
+insert into category values (1,N'Các thứ','Cacthu')
+
